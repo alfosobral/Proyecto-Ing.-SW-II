a1ff5a5a1d9bc27c7018cc17165bc81f13b2f30e
alfosobral <alfonsosobral@gmail.com>
2025-09-09 12:09:46 -0300

    cambios para docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,     // escucha en 0.0.0.0 (necesario en Docker)
+    port: 5173,     // puerto por defecto de Vite
+    proxy: {
+      '/api': {
+        target: 'http://backend:8080', // el servicio "backend" en docker-compose
+        changeOrigin: true,
+      },
+    },
+  },
 })
